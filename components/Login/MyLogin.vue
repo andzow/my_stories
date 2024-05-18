6253244a6edd83637b4bd13ae3242926104f19e8
andzow <andreywsaers@gmail.com>
2024-05-18 12:57:02 +0300

    asdasdads

---
+++ components/Login/MyLogin.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="login">
+    <div class="login__form">
+        <div class="h3">(  вход в панель администратора  )</div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style sc>
+
+</style>
