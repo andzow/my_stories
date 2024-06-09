bd6f66413a5a4daeda9a0d830cc034e5e281ebe3
m1rstan1978 <m1rstan@mail.ru>
2024-06-09 12:42:52 +0300

    fdsf

--- http/services/CategoryServices.js
+++ http/services/CategoryServices.js
@@ -1,25 +1,25 @@
 import $api from "..";
 
 export default class CategoryServices {
-    static async getAll() {
-        return $api.get('/category/get')
-    }
+  static async getAll() {
+    return $api.get("/category/get");
+  }
 
-    static async getCategory() {
-        return $api.get('/category/getCategory', {
-            params: query
-        })
-    }
+  static async getCategory() {
+    return $api.get("/category/getCategory", {
+      params: query,
+    });
+  }
 
-    static async createCategory(FormObject) {
-        return $api.post('/category/create', FormObject)
-    }
+  static async createCategory(FormObject) {
+    return $api.post("/category/create", FormObject);
+  }
 
-    static async deleteCategory() {
-        return $api.delete('/category/delete')
-    }
+  static async deleteCategory() {
+    return $api.delete("/category/delete");
+  }
 
-    static async deleteCategoryOne(id) {
-        return $api.delete('/category/deleteOne', { data: { id } })
-    }
+  static async deleteCategoryOne(id) {
+    return $api.delete("/category/deleteOne", { data: { id } });
+  }
 }
