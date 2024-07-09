24f9e5c660560ba7d2b313363b5ced8f6e6dc847
m1rstan1978 <m1rstan@mail.ru>
2024-07-09 17:00:28 +0300

    аыфв

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,22 +6,17 @@ export default defineNuxtConfig({
     "~/plugins/v-preload.js",
   ],
   css: ["~/assets/style/main.css", "~/assets/style/fonts.css"],
-  // app: {
-  //   pageTransition: {
-  //     name: "page",
-  //     mode: "out-in",
-  //   },
-  // },
+
   routeRules: {
-    // "/": { delayHydration: "mount" },
-    // "/catalog/": { delayHydration: false },
-    // "/catalog/:id/:id/": { delayHydration: false },
+    "/": { delayHydration: "mount" },
+    "/catalog/": { delayHydration: false },
+    "/catalog/:id/:id/": { delayHydration: false },
     "/admin": { ssr: false },
     "/login": { ssr: false },
   },
   ssr: true,
   components: true,
-  modules: ["@nuxt/image"],
+  modules: ["@nuxt/image", "nuxt-delay-hydration"],
   // delayHydration: {
   //   debug: process.env.NODE_ENV === "development",
   // },
