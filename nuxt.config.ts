29be2528b20d481c128b270f864952ab867a5ef1
m1rstan1978 <m1rstan@mail.ru>
2024-07-09 17:02:39 +0300

    куц

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,17 +6,19 @@ export default defineNuxtConfig({
     "~/plugins/v-preload.js",
   ],
   css: ["~/assets/style/main.css", "~/assets/style/fonts.css"],
-
+  app: {
+    pageTransition: {
+      name: "page",
+      mode: "out-in",
+    },
+  },
   routeRules: {
-    "/": { delayHydration: "mount" },
-    "/catalog/": { delayHydration: false },
-    "/catalog/:id/:id/": { delayHydration: false },
     "/admin": { ssr: false },
     "/login": { ssr: false },
   },
   ssr: true,
   components: true,
-  modules: ["@nuxt/image", "nuxt-delay-hydration"],
+  modules: ["@nuxt/image"],
   // delayHydration: {
   //   debug: process.env.NODE_ENV === "development",
   // },
