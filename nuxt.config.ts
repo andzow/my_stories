9d18fb053152ba54f1ff142e3a400d5ff194dae2
m1rstan1978 <m1rstan@mail.ru>
2024-07-10 12:39:42 +0300

    fd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,7 +32,7 @@ export default defineNuxtConfig({
     "/admin": { ssr: false },
     "/login": { ssr: false },
   },
-  ssr: false,
+  ssr: true,
   components: true,
   modules: ["@nuxt/image", "nuxt-delay-hydration", "@nuxtjs/device"],
   delayHydration: {
