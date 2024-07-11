ea4cc703c2a39649c82ccd6e68c197506410accc
m1rstan1978 <m1rstan@mail.ru>
2024-07-11 14:44:29 +0300

    fd

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,7 @@ export default defineNuxtConfig({
   nitro: {
     prerender: {
       crawlLinks: true,
-      routes: ["/sitemap.xml", "/robots.txt"],
+      routes: ["/sitemap.xml"],
     },
   },
   sitemap: {
