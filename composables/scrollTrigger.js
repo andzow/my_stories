5b322f20961b1253f620bf31e89a25911055c28f
m1rstan1978 <m1rstan@mail.ru>
2024-07-12 13:42:06 +0300

    fd

--- composables/scrollTrigger.js
+++ composables/scrollTrigger.js
@@ -23,9 +23,9 @@ export const useGsapAnimationOpacity = (
       arr.forEach((el) => {
         const elHtml = document.querySelector(el);
         if (delayN) {
-          elHtml.style.transition = `opacity 1.5s ease ${delayN}s`;
+          elHtml.style.transition = `opacity 2s linear ${delayN}s`;
         } else {
-          elHtml.style.transition = `opacity 1.5s ease`;
+          elHtml.style.transition = `opacity 2s linear`;
         }
         elHtml.style.opacity = "1";
       });
