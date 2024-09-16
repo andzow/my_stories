0303f7dbfc7cac6ad3a8d8ba55576aafbb44e4a7
m1rstan1978 <m1rstan@mail.ru>
2024-09-16 22:33:35 +0300

    fd

--- app.vue
+++ app.vue
@@ -1,90 +1,91 @@
 <template>
-	<!-- <Transition>
+  <!-- <Transition>
     <UIMyPreloader v-if="!preloader" />
   </Transition> -->
-	<UIMyHeader v-if="headerVisible" />
-	<main class="page" v-lazy-hydrate="() => (checkHydrate = true)">
-		<NuxtPage />
-		<UIMyModalStatus />
-	</main>
-	<UIMyFooter />
+  <NuxtLoadingIndicator throttle="2" height="2" color="#af9280" />
+  <UIMyHeader v-if="headerVisible" />
+  <main class="page" v-lazy-hydrate="() => (checkHydrate = true)">
+    <NuxtPage />
+    <UIMyModalStatus />
+  </main>
+  <UIMyFooter />
 </template>
 
 <script>
-import AuthController from '@/http/controllers/AuthController'
+import AuthController from "@/http/controllers/AuthController";
 
 export default {
-	setup() {
-		const routePath = useRoute().path
-		let checkRoute = false
-		if (routePath === '/login' || routePath === '/admin') {
-			checkRoute = true
-		}
-		return {
-			checkRoute,
-		}
-	},
-	data() {
-		return {
-			useCheckAnimationArr: useCheckAnimationArr(),
-			useCursor: useCursor(),
-			headerVisible: true,
-			preloader: useMainPreload(),
-			checkHydrate: useCheckHydration(),
-			useLengthCart: useLengthCart(),
-			checkMobile: null,
-			cursorFooter: false,
-			useCheckLoadImageCart: useCheckLoadImageCart,
-		}
-	},
-	methods: {
-		async checkAuthApp() {
-			try {
-				await AuthController.cheackAuth()
-			} catch {}
-		},
-		initCartLength() {
-			const cart = JSON.parse(localStorage.getItem('cart'))
-			if (!cart || cart?.length <= 0) {
-				this.useLengthCart = null
-			} else {
-				this.useLengthCart = cart.length
-			}
-		},
-	},
-	mounted() {
-		if (localStorage.getItem('accessToken')) {
-			this.checkAuthApp()
-		}
-		this.initCartLength()
-		if (this.$route.path === '/admin' || this.$route.path === '/login') {
-			this.headerVisible = false
-		} else {
-			this.headerVisible = true
-		}
-		setTimeout(() => {
-			this.preloader = true
-		}, 1000)
-	},
-	watch: {
-		async $route() {
-			const arrLocaleStore = await this.useCheckLoadImageCart(
-				JSON.parse(localStorage.getItem('cart'))
-			)
-			this.useLengthCart = arrLocaleStore.length
-			if (this.$route.path === '/admin' || this.$route.path === '/login') {
-				this.headerVisible = false
-			} else {
-				this.headerVisible = true
-			}
-			setTimeout(() => {
-				if (this.$route.path !== '/lookbook') {
-					this.useCursor = true
-				}
-			}, 500)
-		},
-	},
-}
+  setup() {
+    const routePath = useRoute().path;
+    let checkRoute = false;
+    if (routePath === "/login" || routePath === "/admin") {
+      checkRoute = true;
+    }
+    return {
+      checkRoute,
+    };
+  },
+  data() {
+    return {
+      useCheckAnimationArr: useCheckAnimationArr(),
+      useCursor: useCursor(),
+      headerVisible: true,
+      preloader: useMainPreload(),
+      checkHydrate: useCheckHydration(),
+      useLengthCart: useLengthCart(),
+      checkMobile: null,
+      cursorFooter: false,
+      useCheckLoadImageCart: useCheckLoadImageCart,
+    };
+  },
+  methods: {
+    async checkAuthApp() {
+      try {
+        await AuthController.cheackAuth();
+      } catch {}
+    },
+    initCartLength() {
+      const cart = JSON.parse(localStorage.getItem("cart"));
+      if (!cart || cart?.length <= 0) {
+        this.useLengthCart = null;
+      } else {
+        this.useLengthCart = cart.length;
+      }
+    },
+  },
+  mounted() {
+    if (localStorage.getItem("accessToken")) {
+      this.checkAuthApp();
+    }
+    this.initCartLength();
+    if (this.$route.path === "/admin" || this.$route.path === "/login") {
+      this.headerVisible = false;
+    } else {
+      this.headerVisible = true;
+    }
+    setTimeout(() => {
+      this.preloader = true;
+    }, 1000);
+  },
+  watch: {
+    async $route() {
+      const arrLocaleStore = await this.useCheckLoadImageCart(
+        JSON.parse(localStorage.getItem("cart"))
+      );
+      this.useLengthCart = arrLocaleStore.length;
+      if (this.$route.path === "/admin" || this.$route.path === "/login") {
+        this.headerVisible = false;
+      } else {
+        this.headerVisible = true;
+      }
+      setTimeout(() => {
+        if (this.$route.path !== "/lookbook") {
+          this.useCursor = true;
+        }
+      }, 500);
+    },
+  },
+};
 </script>
 <style>
 /* .pointerCursor {
@@ -94,16 +95,16 @@ export default {
 
 <style scoped>
 .page {
-	position: relative;
-	width: 100%;
-	overflow: hidden;
+  position: relative;
+  width: 100%;
+  overflow: hidden;
 }
 .v-enter-active,
 .v-leave-active {
-	transition: all 0.7s ease-in-out;
+  transition: all 0.7s ease-in-out;
 }
 .v-enter-from,
 .v-leave-to {
-	transform: translateY(-100%);
+  transform: translateY(-100%);
 }
 </style>
