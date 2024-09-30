bf338e1a6d7a6dfaf22779d3f3c387639e966399
m1rstan1978 <m1rstan@mail.ru>
2024-09-30 10:56:07 +0300

    add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,89 +1,91 @@
 export default defineNuxtConfig({
-	devtools: { enabled: false },
-	plugins: [
-		'~/plugins/animate.connect.js',
-		'~/plugins/v-click-outside.js',
-		'~/plugins/v-lazy-loading.js',
-	],
-	css: ['~/assets/style/main.css', '~/assets/style/fonts.css'],
-	nitro: {
-		prerender: {
-			routes: ['/sitemap.xml'],
-		},
-	},
-	app: {
-		// head: {
-		//   title:
-		//     "Интернет-магазин модной женской одежды и доставкой по всей России | MyStories",
-		//   charset: "utf-8",
-		//   viewport: "width=device-width, initial-scale=1",
-		//   link: [
-		//     {
-		//       rel: "icon",
-		//       type: "image/x-icon",
-		//       href: "/favicon.png",
-		//     },
-		//     {
-		//       rel: "preload",
-		//       as: "image",
-		//       href: "/Lookbook/letniy/letniyBack.webp",
-		//     },
-		//     {
-		//       rel: "preload",
-		//       as: "image",
-		//       href: "/Lookbook/awakening/awakeningBack.webp",
-		//     },
-		//   ],
-		//   meta: [
-		//     { property: "og:type", content: "website" },
-		//     { property: "og:locale", content: "ru_RU" },
-		//   ],
-		//   htmlAttrs: { lang: "ru-RU" },
-		// },
-		pageTransition: {
-			name: 'page',
-			mode: 'out-in',
-		},
-	},
-	routeRules: {
-		'/': { delayHydration: 'mount' },
-		'/catalog/**': { delayHydration: false },
-		'/lookbook/**': { delayHydration: false },
-		'/brand/**': { delayHydration: false },
-		'/contacts': { delayHydration: false },
-		'/delivery-and-payment': { delayHydration: false },
-		'/politics': { delayHydration: false },
-		'/order/**': { delayHydration: false },
-		'/dogovor': { delayHydration: false },
-		'/cart': { delayHydration: false },
-		'/admin': { ssr: false },
-		'/login': { ssr: false },
-	},
-	sitemap: {
-		exclude: ['/login', '/admin', '/order', '/order/**', '/cart'],
-		sources: ['/api/sitemap'],
-	},
-	site: {
-		url: 'https://xn--80aqckmmfdf.xn--p1ai',
-	},
-	ssr: true,
-	components: true,
-	modules: [
-		'@nuxt/image',
-		'nuxt-delay-hydration',
-		'@nuxtjs/device',
-		'@nuxtjs/sitemap',
-		'nuxt-multi-cache',
-	],
-	multiCache: {
-		component: {
-			enabled: true,
-		},
-		data: {
-			enabled: true,
-		},
-	},
-	delayHydration: {
-		debug: process.env.NODE_ENV === 'development',
-	},
-})
+  devtools: { enabled: false },
+  plugins: [
+    "~/plugins/animate.connect.js",
+    "~/plugins/v-click-outside.js",
+    "~/plugins/v-lazy-loading.js",
+  ],
+  css: ["~/assets/style/main.css", "~/assets/style/fonts.css"],
+  nitro: {
+    prerender: {
+      routes: ["/sitemap.xml"],
+    },
+  },
+  app: {
+    head: {
+      //   title:
+      //     "Интернет-магазин модной женской одежды и доставкой по всей России | MyStories",
+      //   charset: "utf-8",
+      //   viewport: "width=device-width, initial-scale=1",
+      //   link: [
+      //     {
+      //       rel: "icon",
+      //       type: "image/x-icon",
+      //       href: "/favicon.png",
+      //     },
+      //     {
+      //       rel: "preload",
+      //       as: "image",
+      //       href: "/Lookbook/letniy/letniyBack.webp",
+      //     },
+      //     {
+      //       rel: "preload",
+      //       as: "image",
+      //       href: "/Lookbook/awakening/awakeningBack.webp",
+      //     },
+      //   ],
+      meta: [
+        {
+          name: "yandex-verification",
+          content: "6322280b4536da2c",
+        },
+      ],
+      //   htmlAttrs: { lang: "ru-RU" },
+    },
+    pageTransition: {
+      name: "page",
+      mode: "out-in",
+    },
+  },
+  routeRules: {
+    "/": { delayHydration: "mount" },
+    "/catalog/**": { delayHydration: false },
+    "/lookbook/**": { delayHydration: false },
+    "/brand/**": { delayHydration: false },
+    "/contacts": { delayHydration: false },
+    "/delivery-and-payment": { delayHydration: false },
+    "/politics": { delayHydration: false },
+    "/order/**": { delayHydration: false },
+    "/dogovor": { delayHydration: false },
+    "/cart": { delayHydration: false },
+    "/admin": { ssr: false },
+    "/login": { ssr: false },
+  },
+  sitemap: {
+    exclude: ["/login", "/admin", "/order", "/order/**", "/cart"],
+    sources: ["/api/sitemap"],
+  },
+  site: {
+    url: "https://xn--80aqckmmfdf.xn--p1ai",
+  },
+  ssr: true,
+  components: true,
+  modules: [
+    "@nuxt/image",
+    "nuxt-delay-hydration",
+    "@nuxtjs/device",
+    "@nuxtjs/sitemap",
+    "nuxt-multi-cache",
+  ],
+  multiCache: {
+    component: {
+      enabled: true,
+    },
+    data: {
+      enabled: true,
+    },
+  },
+  delayHydration: {
+    debug: process.env.NODE_ENV === "development",
+  },
+});
--- pages/order/index.vue
+++ pages/order/index.vue
@@ -1,119 +1,119 @@
 <template>
-	<div>
-		<OrderMyMain />
-		<Transition>
-			<OrderMyDeliveryPvz
-				v-if="usePvzModal"
-				@keydown.esc="usePvzModal = false"
-			/>
-		</Transition>
+  <div>
+    <OrderMyMain />
+    <Transition>
+      <OrderMyDeliveryPvz
+        v-if="usePvzModal"
+        @keydown.esc="usePvzModal = false"
+      />
+    </Transition>
 
-		<LazyUIMyAnimationLine
-			duration="7"
-			:arrAnimationLine="arrAnimationLine"
-			v-if="arrAnimationLine && checkHydrate"
-		/>
-		<LazyUIMyNoise v-if="checkHydrate" />
-	</div>
+    <LazyUIMyAnimationLine
+      duration="7"
+      :arrAnimationLine="arrAnimationLine"
+      v-if="arrAnimationLine && checkHydrate"
+    />
+    <LazyUIMyNoise v-if="checkHydrate" />
+  </div>
 </template>
 
 <script>
 export default {
-	setup() {
-		useHead({
-			title:
-				'Оформление заказа - Интернет-магазин модной женской одежды и доставкой по всей России | MyStories',
-			charset: 'utf-8',
-			viewport: 'width=device-width, initial-scale=1',
-			link: [
-				{
-					rel: 'icon',
-					type: 'image/x-icon',
-					href: '/favicon.png',
-				},
-			],
-			meta: [
-				{ property: 'og:type', content: 'website' },
-				{ property: 'og:locale', content: 'ru_RU' },
-			],
-			charset: 'utf-8',
-			htmlAttrs: { lang: 'ru-RU' },
-			script: [
-				{
-					src: 'https://api-maps.yandex.ru/2.1/?apikey=3170b455-c141-4e3e-a844-79b04a6021eb&lang=ru_RU',
-				},
-			],
-		})
-	},
-	data() {
-		return {
-			usePvzModal: usePvzModal(),
-			usePvzCode: usePvzCode(),
-			arrAnimationLine: null,
-			checkHydrate: useCheckHydration(),
-		}
-	},
-	methods: {
-		initGsap() {
-			if (window.innerWidth > 936) {
-				this.arrAnimationLine = [
-					{ name: '.header__logo', defaultLine: false, indent: 'left' },
-					{
-						name: '.main__info',
-						defaultLine: false,
-						indent: 'left',
-						widthTo: true,
-					},
-					{
-						name: '.header__interaction_search',
-						defaultLine: false,
-						indent: 'left',
-					},
-				]
-			} else {
-				this.arrAnimationLine = [
-					{ name: '.header__logo', defaultLine: false, indent: 'left' },
-					{
-						name: '.header__interaction_cart',
-						defaultLine: false,
-						indent: 'left',
-						widthTo: true,
-					},
-				]
-			}
-		},
-	},
-	beforeMount() {
-		if (
-			!localStorage.getItem('cart') ||
-			localStorage.getItem('cart')?.length <= 0 ||
-			JSON.parse(localStorage.getItem('cart'))?.length <= 0
-		) {
-			this.$router.push('/page/not-found')
-		}
-	},
-	unmounted() {
-		this.usePvzCode = null
-	},
-	mounted() {
-		const bodyEl = document.body
-		bodyEl.style.overflow = 'auto'
-		setTimeout(() => {
-			this.initGsap()
-		}, 0)
-	},
-}
+  setup() {
+    useHead({
+      title:
+        "Оформление заказа - Интернет-магазин модной женской одежды и доставкой по всей России | MyStories",
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      link: [
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/favicon.png",
+        },
+      ],
+      meta: [
+        { property: "og:type", content: "website" },
+        { property: "og:locale", content: "ru_RU" },
+      ],
+      charset: "utf-8",
+      htmlAttrs: { lang: "ru-RU" },
+      script: [
+        {
+          src: "https://api-maps.yandex.ru/2.1/?apikey=48d4c7cd-889b-4b78-ae4d-5ee5efd8945e&lang=ru_RU",
+        },
+      ],
+    });
+  },
+  data() {
+    return {
+      usePvzModal: usePvzModal(),
+      usePvzCode: usePvzCode(),
+      arrAnimationLine: null,
+      checkHydrate: useCheckHydration(),
+    };
+  },
+  methods: {
+    initGsap() {
+      if (window.innerWidth > 936) {
+        this.arrAnimationLine = [
+          { name: ".header__logo", defaultLine: false, indent: "left" },
+          {
+            name: ".main__info",
+            defaultLine: false,
+            indent: "left",
+            widthTo: true,
+          },
+          {
+            name: ".header__interaction_search",
+            defaultLine: false,
+            indent: "left",
+          },
+        ];
+      } else {
+        this.arrAnimationLine = [
+          { name: ".header__logo", defaultLine: false, indent: "left" },
+          {
+            name: ".header__interaction_cart",
+            defaultLine: false,
+            indent: "left",
+            widthTo: true,
+          },
+        ];
+      }
+    },
+  },
+  beforeMount() {
+    if (
+      !localStorage.getItem("cart") ||
+      localStorage.getItem("cart")?.length <= 0 ||
+      JSON.parse(localStorage.getItem("cart"))?.length <= 0
+    ) {
+      this.$router.push("/page/not-found");
+    }
+  },
+  unmounted() {
+    this.usePvzCode = null;
+  },
+  mounted() {
+    const bodyEl = document.body;
+    bodyEl.style.overflow = "auto";
+    setTimeout(() => {
+      this.initGsap();
+    }, 0);
+  },
+};
 </script>
 
 <style scoped>
 .v-enter-active,
 .v-leave-active {
-	transition: opacity transform 0.5s ease;
+  transition: opacity transform 0.5s ease;
 }
 
 .v-enter-from,
 .v-leave-to {
-	opacity: 0;
-	transform: translateY(-20px);
+  opacity: 0;
+  transform: translateY(-20px);
 }
 </style>
