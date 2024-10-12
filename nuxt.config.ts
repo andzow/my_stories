60fa1744043901dea1eff976f27bbb525ebba166
m1rstan1978 <m1rstan@mail.ru>
2024-10-13 00:40:19 +0300

    add metr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,37 +13,58 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      //   title:
-      //     "Интернет-магазин модной женской одежды и доставкой по всей России | MyStories",
-      //   charset: "utf-8",
-      //   viewport: "width=device-width, initial-scale=1",
-      //   link: [
-      //     {
-      //       rel: "icon",
-      //       type: "image/x-icon",
-      //       href: "/favicon.png",
-      //     },
-      //     {
-      //       rel: "preload",
-      //       as: "image",
-      //       href: "/Lookbook/letniy/letniyBack.webp",
-      //     },
-      //     {
-      //       rel: "preload",
-      //       as: "image",
-      //       href: "/Lookbook/awakening/awakeningBack.webp",
-      //     },
-      //   ],
-      meta: [
+      // title:
+      //   "Интернет-магазин модной женской одежды и доставкой по всей России | MyStories",
+      // charset: "utf-8",
+      // viewport: "width=device-width, initial-scale=1",
+      // link: [
+      //   {
+      //     rel: "icon",
+      //     type: "image/x-icon",
+      //     href: "/favicon.png",
+      //   },
+      //   {
+      //     rel: "preload",
+      //     as: "image",
+      //     href: "/Lookbook/letniy/letniyBack.webp",
+      //   },
+      //   {
+      //     rel: "preload",
+      //     as: "image",
+      //     href: "/Lookbook/awakening/awakeningBack.webp",
+      //   },
+      // ],script: [
+      script: [
         {
-          name: "yandex-verification",
-          content: "5a131032bb264c6a",
-        },
-        {
-          name: "google-site-verification",
-          content: "NR92c5qUhMC4FIIKMI1j_tARazWwxOMpdg23B0KEBnI",
+          type: "text/javascript",
+          innerHTML: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(98619510, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });`,
         },
       ],
+      noscript: [
+        {
+          innerHTML: `<div><img src="https://mc.yandex.ru/watch/98619510" style="position:absolute; left:-9999px;" alt="" /></div>`,
+        },
+      ],
+      // meta: [
+      //   {
+      //     name: "yandex-verification",
+      //     content: "5a131032bb264c6a",
+      //   },
+      //   {
+      //     name: "google-site-verification",
+      //     content: "NR92c5qUhMC4FIIKMI1j_tARazWwxOMpdg23B0KEBnI",
+      //   },
+      // ],
       //   htmlAttrs: { lang: "ru-RU" },
     },
     pageTransition: {
